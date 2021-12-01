0ac94c4d2d6c5d8dad87bd703a8f10b0e973ca7c
JackHeeney <jackheeney1@googlemail.com>
2021-12-01 14:50:52 +0000

    First commit

---
+++ assets/inc/components/nav.php
@@ -0,0 +1,137 @@
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Robust IT</h5>
+        <button type="button" class="btn-close text-reset float-end" data-bs-dismiss="offcanvas"
+            aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <!-- <div>
+            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists,
+            etc.
+        </div> -->
+
+        <!-- mobile view -->
+        <div class=" navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<header class="container-fluid bg-dark navbar-light nav-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <span class="icon email float-end"><a
+                        href="mailto:info@robustittraining.com">info@robustittraining.com</a></span>
+                <span class="icon phone float-end"><a href="tel:02038757827">02038 757 827 </a></span>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="container-fluid navbar-light bg-white shadow">
+    <div class="container">
+        <nav class="navbar sticky-top navbar-expand-lg ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img
+                        src="<?php echo SITE_ADDR ?>/assets/img/microsoft-gold-partner.jpg" class="img-fluid"
+                        alt="microsoft gold partners"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    All Courses
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">Server</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Cloud</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Security</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Azure</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">SQL</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Coding</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Desktop</a></li>
+
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Search <i class="fas fa-search"></i></a>
+                            </li>
+
+                        </ul>
+
+
+
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+// Change page title and meta tags here
+$title = "Microsoft Certified Training | Robust IT Training";
+$meta['keywords'] = "";
+$meta['description'] = "";
+
+// Social media meta tags
+$meta['og:title'] = "Microsoft Certified Training | Robust IT Training";
+$meta['og:description'] = "";
+$meta['og:image'] = "";
+$meta['og:url'] = "";
+
+?>
+
+<?php include './assets/inc/header.php' ?>
+<?php include './assets/inc/components/nav.php' ?>
+<?php include './assets/inc/components/hero.php' ?>
+
+
+<main id="home" class="container-fluid">
+
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+                        <img src="<? echo SITE_ADDR ?>/assets/img/cards/card-1.png" class="card-img img-fluid"
+                            alt="hacker icon green">
+                    </div>
+                    <div class="card-body">
+                        <h2>Heading One</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum voluptatem quaerat nihil in,
+                            a repudiandae odit veritatis nesciunt harum quia excepturi culpa adipisci quisquam
+                            provident, suscipit nam blanditiis, quo assumenda!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Heading One</h2>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum voluptatem quaerat nihil in,
+                            a repudiandae odit veritatis nesciunt harum quia excepturi culpa adipisci quisquam
+                            provident, suscipit nam blanditiis, quo assumenda!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Heading One</h2>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum voluptatem quaerat nihil in,
+                            a repudiandae odit veritatis nesciunt harum quia excepturi culpa adipisci quisquam
+                            provident, suscipit nam blanditiis, quo assumenda!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
+
+
+
+<?php include './assets/inc/footer.php' ?>
